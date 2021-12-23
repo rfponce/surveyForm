609e48ad5c148289b3838f0479a76c7a19c18e43
Fabián Ponce <maximohumano@gmail.com>
2021-12-23 14:21:39 -0600

    Display the information in an alert()

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function captureInformation()
+{
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const age = document.getElementById("number").value;
+  const sex = document.querySelector("input[name='sex']:checked").value;
+  const ownMoreComputers = document.getElementById("dropdown").value;
+  const osList = document.querySelectorAll("input[name='os']:checked");
+  const comment = document.getElementById("comments").value;
+  let message = "";
+
+  message += "Your information was...\n";
+  message += "Name: " + name + "\n";
+  message += "Email: " + email + "\n";
+  if(message) message += "Age: " + age + "\n";
+  message += "Sex: " + sex + "\n";
+  message += "Do you own more than one computer?: " + ownMoreComputers + "\n";
+  message += "Operating systems you use: \n";
+
+  for(let i = 0; i < osList.length; i++)
+  {
+    message += "- " + osList[i].value + "\n";
+  }
+
+  if(comment) message += "Comment: " + comment + "\n";
+  return message;
+}
